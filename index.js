b2eef44493a4ffd0eab0724dfacf0d717e592eac
Matheus Barbosa <matheus_barbosa@usp.br>
2019-08-05 14:07:31 -0300

    hi

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require ('fs');
+const app = express();
+
+app.use('/static', express.static('static'));
+app.use(bodyParser.urlencoded());
+app.set('view engine', 'ejs');
+
+
+app.get('', (req, res) => {
+    res.render('index');
+});
+
+app.get('/secondpage', (req, res) => {
+    res.render('secondpage'); 
+});
+
+
+app.listen(3000, () => {
+    console.log('Servidor inicializado na 3000')
+});
